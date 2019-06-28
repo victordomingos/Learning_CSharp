69c7f33a5c79a3697b5ae21c38accf65fc33c14b
victordomingos <editor.arcosonline@gmail.com>
2019-06-28 01:14:07 +0100

    Prod1

--- Prod1_Shrek_da_Silva/Form1.Designer.cs
+++ Prod1_Shrek_da_Silva/Form1.Designer.cs
@@ -47,13 +47,15 @@
             this.cmb_concelho = new System.Windows.Forms.ComboBox();
             this.btn_calcular = new System.Windows.Forms.Button();
             this.txt_resultado = new System.Windows.Forms.TextBox();
+            this.lbl_ano_erro = new System.Windows.Forms.Label();
+            this.lbl_valor_base_erro = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 89);
+            this.label1.Location = new System.Drawing.Point(14, 94);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
@@ -62,7 +64,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 118);
+            this.label2.Location = new System.Drawing.Point(14, 123);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(98, 13);
             this.label2.TabIndex = 1;
@@ -71,7 +73,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(14, 144);
+            this.label3.Location = new System.Drawing.Point(14, 149);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(57, 13);
             this.label3.TabIndex = 2;
@@ -79,7 +81,9 @@
             // 
             // txt_nome
             // 
-            this.txt_nome.Location = new System.Drawing.Point(113, 86);
+            this.txt_nome.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_nome.Location = new System.Drawing.Point(113, 91);
             this.txt_nome.Name = "txt_nome";
             this.txt_nome.Size = new System.Drawing.Size(267, 20);
             this.txt_nome.TabIndex = 3;
@@ -87,29 +91,30 @@
             // 
             // txt_ano
             // 
-            this.txt_ano.Location = new System.Drawing.Point(113, 115);
+            this.txt_ano.Location = new System.Drawing.Point(113, 120);
             this.txt_ano.Name = "txt_ano";
             this.txt_ano.Size = new System.Drawing.Size(100, 20);
             this.txt_ano.TabIndex = 4;
-            this.txt_ano.Text = "2001";
+            this.txt_ano.TextChanged += new System.EventHandler(this.Txt_ano_TextChanged);
             // 
             // txt_valor_base
             // 
-            this.txt_valor_base.Location = new System.Drawing.Point(113, 141);
+            this.txt_valor_base.Location = new System.Drawing.Point(113, 146);
             this.txt_valor_base.Name = "txt_valor_base";
             this.txt_valor_base.Size = new System.Drawing.Size(100, 20);
             this.txt_valor_base.TabIndex = 5;
-            this.txt_valor_base.Text = "100";
+            this.txt_valor_base.TextChanged += new System.EventHandler(this.Txt_valor_base_TextChanged);
             // 
             // chk_escalao10
             // 
             this.chk_escalao10.AutoSize = true;
-            this.chk_escalao10.Location = new System.Drawing.Point(113, 181);
+            this.chk_escalao10.Location = new System.Drawing.Point(113, 186);
             this.chk_escalao10.Name = "chk_escalao10";
             this.chk_escalao10.Size = new System.Drawing.Size(162, 17);
             this.chk_escalao10.TabIndex = 6;
             this.chk_escalao10.Text = "Desconto de escalão? (10%)";
             this.chk_escalao10.UseVisualStyleBackColor = true;
+            this.chk_escalao10.CheckedChanged += new System.EventHandler(this.Chk_escalao10_CheckedChanged);
             // 
             // label4
             // 
@@ -120,13 +125,15 @@
             this.label4.Size = new System.Drawing.Size(373, 31);
             this.label4.TabIndex = 7;
             this.label4.Text = "Cálculo do Valor da Avença";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(442, 86);
+            this.pictureBox1.Location = new System.Drawing.Point(455, 91);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(157, 141);
+            this.pictureBox1.Size = new System.Drawing.Size(157, 145);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
@@ -134,27 +141,29 @@
             // chk_desconto_socio_5
             // 
             this.chk_desconto_socio_5.AutoSize = true;
-            this.chk_desconto_socio_5.Location = new System.Drawing.Point(113, 204);
+            this.chk_desconto_socio_5.Location = new System.Drawing.Point(113, 209);
             this.chk_desconto_socio_5.Name = "chk_desconto_socio_5";
             this.chk_desconto_socio_5.Size = new System.Drawing.Size(144, 17);
             this.chk_desconto_socio_5.TabIndex = 9;
             this.chk_desconto_socio_5.Text = "Desconto de sócio? (5%)";
             this.chk_desconto_socio_5.UseVisualStyleBackColor = true;
+            this.chk_desconto_socio_5.CheckedChanged += new System.EventHandler(this.Chk_desconto_socio_5_CheckedChanged);
             // 
             // chk_agravamento_12e
             // 
             this.chk_agravamento_12e.AutoSize = true;
-            this.chk_agravamento_12e.Location = new System.Drawing.Point(113, 228);
+            this.chk_agravamento_12e.Location = new System.Drawing.Point(113, 233);
             this.chk_agravamento_12e.Name = "chk_agravamento_12e";
             this.chk_agravamento_12e.Size = new System.Drawing.Size(148, 17);
             this.chk_agravamento_12e.TabIndex = 10;
             this.chk_agravamento_12e.Text = "Tem agravamento? (12 €)";
             this.chk_agravamento_12e.UseVisualStyleBackColor = true;
+            this.chk_agravamento_12e.CheckedChanged += new System.EventHandler(this.Chk_agravamento_12e_CheckedChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 272);
+            this.label5.Location = new System.Drawing.Point(12, 277);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(94, 13);
             this.label5.TabIndex = 11;
@@ -163,29 +172,31 @@
             // rad_sim
             // 
             this.rad_sim.AutoSize = true;
-            this.rad_sim.Location = new System.Drawing.Point(112, 269);
+            this.rad_sim.Location = new System.Drawing.Point(113, 275);
             this.rad_sim.Name = "rad_sim";
             this.rad_sim.Size = new System.Drawing.Size(42, 17);
             this.rad_sim.TabIndex = 12;
             this.rad_sim.TabStop = true;
             this.rad_sim.Text = "Sim";
             this.rad_sim.UseVisualStyleBackColor = true;
+            this.rad_sim.CheckedChanged += new System.EventHandler(this.Rad_sim_CheckedChanged);
             // 
             // rad_nao
             // 
             this.rad_nao.AutoSize = true;
-            this.rad_nao.Location = new System.Drawing.Point(112, 293);
+            this.rad_nao.Location = new System.Drawing.Point(113, 299);
             this.rad_nao.Name = "rad_nao";
             this.rad_nao.Size = new System.Drawing.Size(45, 17);
             this.rad_nao.TabIndex = 13;
             this.rad_nao.TabStop = true;
             this.rad_nao.Text = "Não";
             this.rad_nao.UseVisualStyleBackColor = true;
+            this.rad_nao.CheckedChanged += new System.EventHandler(this.Rad_nao_CheckedChanged);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(16, 341);
+            this.label6.Location = new System.Drawing.Point(16, 346);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(52, 13);
             this.label6.TabIndex = 14;
@@ -193,19 +204,23 @@
             // 
             // cmb_concelho
             // 
+            this.cmb_concelho.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.cmb_concelho.FormattingEnabled = true;
             this.cmb_concelho.Items.AddRange(new object[] {
             "Amares",
             "Braga",
             "Guimarães"});
-            this.cmb_concelho.Location = new System.Drawing.Point(112, 338);
+            this.cmb_concelho.Location = new System.Drawing.Point(112, 343);
             this.cmb_concelho.Name = "cmb_concelho";
             this.cmb_concelho.Size = new System.Drawing.Size(162, 21);
             this.cmb_concelho.TabIndex = 15;
+            this.cmb_concelho.SelectedIndexChanged += new System.EventHandler(this.Cmb_concelho_SelectedIndexChanged);
             // 
             // btn_calcular
             // 
-            this.btn_calcular.Location = new System.Drawing.Point(384, 336);
+            this.btn_calcular.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_calcular.Location = new System.Drawing.Point(384, 341);
             this.btn_calcular.Name = "btn_calcular";
             this.btn_calcular.Size = new System.Drawing.Size(75, 23);
             this.btn_calcular.TabIndex = 16;
@@ -215,16 +230,41 @@
             // 
             // txt_resultado
             // 
-            this.txt_resultado.Location = new System.Drawing.Point(466, 338);
+            this.txt_resultado.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_resultado.Location = new System.Drawing.Point(466, 343);
             this.txt_resultado.Name = "txt_resultado";
-            this.txt_resultado.Size = new System.Drawing.Size(122, 20);
+            this.txt_resultado.Size = new System.Drawing.Size(146, 20);
             this.txt_resultado.TabIndex = 17;
+            // 
+            // lbl_ano_erro
+            // 
+            this.lbl_ano_erro.AutoSize = true;
+            this.lbl_ano_erro.ForeColor = System.Drawing.Color.Red;
+            this.lbl_ano_erro.Location = new System.Drawing.Point(216, 124);
+            this.lbl_ano_erro.Name = "lbl_ano_erro";
+            this.lbl_ano_erro.Size = new System.Drawing.Size(31, 13);
+            this.lbl_ano_erro.TabIndex = 18;
+            this.lbl_ano_erro.Text = "MSG";
+            this.lbl_ano_erro.Visible = false;
+            // 
+            // lbl_valor_base_erro
+            // 
+            this.lbl_valor_base_erro.AutoSize = true;
+            this.lbl_valor_base_erro.ForeColor = System.Drawing.Color.Red;
+            this.lbl_valor_base_erro.Location = new System.Drawing.Point(216, 149);
+            this.lbl_valor_base_erro.Name = "lbl_valor_base_erro";
+            this.lbl_valor_base_erro.Size = new System.Drawing.Size(31, 13);
+            this.lbl_valor_base_erro.TabIndex = 19;
+            this.lbl_valor_base_erro.Text = "MSG";
+            this.lbl_valor_base_erro.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(619, 389);
+            this.ClientSize = new System.Drawing.Size(624, 375);
+            this.Controls.Add(this.lbl_valor_base_erro);
+            this.Controls.Add(this.lbl_ano_erro);
             this.Controls.Add(this.txt_resultado);
             this.Controls.Add(this.btn_calcular);
             this.Controls.Add(this.cmb_concelho);
@@ -243,8 +283,9 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(640, 38);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Associação Cultural do Pântano";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -271,6 +312,8 @@
         private System.Windows.Forms.ComboBox cmb_concelho;
         private System.Windows.Forms.Button btn_calcular;
         private System.Windows.Forms.TextBox txt_resultado;
+        private System.Windows.Forms.Label lbl_ano_erro;
+        private System.Windows.Forms.Label lbl_valor_base_erro;
     }
 }
 
--- Prod1_Shrek_da_Silva/Form1.cs
+++ Prod1_Shrek_da_Silva/Form1.cs
@@ -1,41 +1,165 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿// Produção 1 (C#) - Cálculo do Valor da Avença
+// 2019-06-27 
+// Victor Domingos
+
+using System;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Prod1_Shrek_da_Silva
 {
     public partial class Form1 : Form
     {
+        // se ano de nascimento for 2000 ou superior, tem desconto de 10 % (passa a 90)
+        // se desconto de escalão ativo, desconta outros 10 % sobre 100(passa a 80)
+        // se desconto de sócio ativo, desconta 5 % (75)
+        // se tem agravamento, acresce 12(não é percentagem)(87)
+        // se reside no distrito desconta 1 % (86) 
+        // se o concelho for Amares, desconta 20 % (66)(considere Braga, Amares e Guimarães, como concelhos para a combobox)
+
+        private readonly double percent_desc_ano = 10;
+        private readonly double percent_desc_escalao = 10;
+        private readonly double percent_desc_socio = 5;
+        private readonly double valor_agravamento = 12;
+        private readonly double percent_desc_reside_distrito = 1;
+        private readonly double percent_desc_reside_amares = 20;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Btn_calcular_Click(object sender, EventArgs e)
+        // Calcula o montante do desconto a partir do valor base e da percentagem do desconto
+        // Ex. Valor base 200, com 10 % desconto --> 20
+        private double CalcularDesconto(double valor_base, double percent_desc)
         {
-            Int64 valor_base;
-            Int16 ano;
-            Double desc_escalao = 0.90;
-            Double desc_socio = 0.95;
-            Int64 agravamento = 12;
-            Double reside_distrito = 0.99;
-            Double reside_amares = 1.20;
-            Int64 resultado;
-
-            valor_base = Convert.ToInt64(txt_valor_base.Text);
-            
-            
+            return valor_base * percent_desc / 100;
+        }
 
 
+        private bool IsFormValid()
+        {
+            bool form_status = true;
+            int ano_atual = System.DateTime.Now.Year;
+            int ano;
+            double valor_base;
+            Color bad_field_color = Color.LightSalmon;
 
-            resultado = (Int64) (valor_base * desc_escalao * desc_socio * reside_distrito * reside_amares + agravamento);
+            try
+            {
+                ano = Convert.ToInt32(txt_ano.Text);
+                
+                if ((ano > ano_atual) || (ano < ano_atual - 150))
+                {
+                    // ano fora do intervalo.
+                    txt_ano.BackColor = bad_field_color;
+                    lbl_ano_erro.Text = "O ano encontra-se fora do intervalo.";
+                    lbl_ano_erro.Visible = true;
+                    form_status = false;
+                }
+                else
+                {
+                    txt_ano.BackColor = Color.White;
+                    lbl_ano_erro.Visible = false;
+                }
+            }
+            catch (Exception)
+            {
+                lbl_ano_erro.Text = "Por favor introduza um número válido.";
+                lbl_ano_erro.Visible = true;
+                txt_ano.BackColor = bad_field_color;
+                form_status = false;
+            }
+
+
+            try
+            {
+                valor_base = Convert.ToDouble(txt_valor_base.Text);
+
+                if (valor_base <= 0)
+                {
+                    // valor base fora do intervalo.
+                    txt_valor_base.BackColor = bad_field_color;
+                    lbl_valor_base_erro.Text = "O valor base deve ser positivo.";
+                    lbl_valor_base_erro.Visible = true;
+                    form_status = false;
+                }
+                else
+                {
+                    txt_valor_base.BackColor = Color.White;
+                    lbl_valor_base_erro.Visible = false;
+                }
+            }
+            catch (Exception)
+            {
+                txt_valor_base.BackColor = bad_field_color;
+                lbl_valor_base_erro.Text = "Por favor introduza um número válido.";
+                lbl_valor_base_erro.Visible = true;
+                form_status = false;
+            }
+
+            // reside distrito ?
+            // concelho ?
+
+            return form_status;
+        }
+
+
+        private void CalcularTudo()
+        {
+            double valor_base;
+            double resultado;
+            short ano;
+
+            if (!IsFormValid())
+                return;
+
+            // Descontos e agravamentos começam a zero e só são alterados se for necessário.
+            double desc_ano = 0;
+            double desc_escalao = 0;
+            double desc_socio = 0;
+            double agravamento = 0;
+            double desc_reside_distrito = 0;
+            double desc_reside_amares = 0;
+
+            valor_base = Convert.ToDouble(txt_valor_base.Text);
+            ano = Convert.ToInt16(txt_ano.Text);
+
+
+            // Alterar os valores de descontos e agravamentos de acordo com input
+            if (ano >= 2000)
+                desc_ano = CalcularDesconto(valor_base, percent_desc_ano);
+
+            if (chk_escalao10.Checked)
+                desc_escalao = CalcularDesconto(valor_base, percent_desc_escalao);
+
+            if (chk_desconto_socio_5.Checked)
+                desc_socio = CalcularDesconto(valor_base, percent_desc_socio);
+
+            if (chk_agravamento_12e.Checked)
+                agravamento = valor_agravamento;
+
+            if (rad_sim.Checked)
+                desc_reside_distrito = CalcularDesconto(valor_base, percent_desc_reside_distrito);
+
+            if (cmb_concelho.Text == "Amares")
+                desc_reside_amares = CalcularDesconto(valor_base, percent_desc_reside_amares);
+
+
+            // Calcular e mostrar resultado
+            resultado = valor_base - desc_ano - desc_escalao - desc_socio - desc_reside_distrito - desc_reside_amares + agravamento;
             txt_resultado.Text = Convert.ToString(resultado);
         }
+
+
+        private void Btn_calcular_Click(object sender, EventArgs e) { CalcularTudo(); }
+        private void Txt_ano_TextChanged(object sender, EventArgs e) { CalcularTudo(); }
+        private void Txt_valor_base_TextChanged(object sender, EventArgs e) { CalcularTudo(); }
+        private void Chk_escalao10_CheckedChanged(object sender, EventArgs e) { CalcularTudo(); }
+        private void Chk_desconto_socio_5_CheckedChanged(object sender, EventArgs e) { CalcularTudo(); }
+        private void Chk_agravamento_12e_CheckedChanged(object sender, EventArgs e) { CalcularTudo(); }
+        private void Rad_sim_CheckedChanged(object sender, EventArgs e) { CalcularTudo(); }
+        private void Rad_nao_CheckedChanged(object sender, EventArgs e) { CalcularTudo(); }
+        private void Cmb_concelho_SelectedIndexChanged(object sender, EventArgs e) { CalcularTudo(); }
     }
 }
