03c68da067bad9d9079942977ee39b2319a86dac
Victor Domingos <editor.arcosonline@gmail.com>
2019-07-30 15:12:49 +0100

    typo

--- 05_Prod2_produtos_e_categorias/config.cs
+++ 05_Prod2_produtos_e_categorias/config.cs
@@ -1,6 +1,6 @@
 namespace _05_Prod2_produtos_e_categorias
 {
-    class config
+    class Config
     {
         // Fill in database details...
         public static readonly string SC = "Data Source=;" +
