7164bb1169b837c461bfa18fc835f7bfbda70aa9
Victor Domingos <victordomingos@users.noreply.github.com>
2019-07-30 14:39:58 +0100

    Update config.cs

--- 05_Prod2_produtos_e_categorias/config.cs
+++ 05_Prod2_produtos_e_categorias/config.cs
@@ -1,9 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace _05_Prod2_produtos_e_categorias
 {
     class config
