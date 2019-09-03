3b76942ed94f4d0961be9644429d091176fa03ba
Victor Domingos <editor.arcosonline@gmail.com>
2019-09-03 10:30:13 +0100

    VS project

---
+++ 06_frota/Frota/config.cs
@@ -0,0 +1,13 @@
+
+namespace Frota
+{
+    class Config
+    {
+        // Fill in database details...
+        public static readonly string SC = "Data Source=;" +
+                                         @"Initial Catalog=;" +
+                                         @"User ID=;" +
+                                         @"Password=;";
+    }
+}
+
