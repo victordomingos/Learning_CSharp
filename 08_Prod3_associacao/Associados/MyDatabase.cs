900b848cb5e9a7bb7160f1d57787b87d9e536def
Victor Domingos <editor.arcosonline@gmail.com>
2019-09-17 09:56:25 +0100

    db setup

---
+++ 08_Prod3_associacao/Associados/MyDatabase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Associados
+{
+    public class MyDatabase
+    {
+        public DataTable ObterDados(string ssql)
+        {
+            using (SqlConnection C = new SqlConnection(Config.SC))
+            {
+                SqlCommand command = C.CreateCommand();
+                command.CommandText = ssql;
+                SqlDataAdapter da = new SqlDataAdapter(command);
+                var dt = new DataTable();
+                da.Fill(dt);
+                C.Close();
+                return dt;
+            }
+        }
+
+
+
+
+    }
+}
---
+++ 08_Prod3_associacao/Associados/config.cs
@@ -0,0 +1,16 @@
+
+namespace Associados
+{
+    class Config
+    {
+        // Fill in database details...
+        public static readonly string SC = "Data Source=;" +
+                                          @"Initial Catalog=;" +
+                                          @"User ID=;" +
+                                          @"Password=;" +
+                                          @"Connect Timeout = 15;";
+    }
+
+
+}
+
